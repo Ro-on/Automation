0d761c4ee955488bd889e60720b5413111b4e3fd
Valeria Zibrova <Vale.ro.on@yandex.ru>
2025-05-24 22:48:40 +0300

    Add time

--- 3.6 - Pytest/test_items.py
+++ 3.6 - Pytest/test_items.py
@@ -4,6 +4,5 @@ import time
 def test_should_be_basket_button(r_browser):
      link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
      r_browser.get(link)
-     
      assert r_browser.find_element(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket"), "Basket button not found"
      time.sleep(5)
